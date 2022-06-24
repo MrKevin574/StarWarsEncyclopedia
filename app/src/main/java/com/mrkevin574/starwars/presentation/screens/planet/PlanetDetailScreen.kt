0e9f38a4690c1cebb5d6feef40a9437fbf6ad040
MrKevin574 <mrkevin574@gmail.com>
2022-06-24 14:54:23 -0400

    Finalizing planet detail screen

--- app/src/main/java/com/mrkevin574/starwars/presentation/screens/planet/PlanetDetailScreen.kt
+++ app/src/main/java/com/mrkevin574/starwars/presentation/screens/planet/PlanetDetailScreen.kt
@@ -1,10 +1,54 @@
 package com.mrkevin574.starwars.presentation.screens.planet
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mrkevin574.starwars.R
 import com.mrkevin574.starwars.domain.model.Planet
+import com.mrkevin574.starwars.presentation.screens.film.ComponentDetail
+import com.mrkevin574.starwars.presentation.ui.theme.YellowStarWars
+import com.mrkevin574.starwars.presentation.ui.theme.starWarsFont
 
 @Composable
 fun PlanetDetailScreen(planet: Planet)
 {
+    LazyColumn(modifier =
+    Modifier.fillMaxSize().background(Color.Black),
+        verticalArrangement = Arrangement.Center
+    ) {
+        item{
+            Text(
+                text = planet.name,
+                color = YellowStarWars,
+                fontFamily = starWarsFont,
+                fontSize = 28.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(30.dp)
+                    .fillMaxWidth()
+            )
+            ComponentDetail(type = stringResource(R.string.rotation_period), value = planet.rotation_period)
+            ComponentDetail(type = stringResource(R.string.orbital_period), value = planet.orbital_period)
+            ComponentDetail(type = stringResource(R.string.diameter), value = planet.diameter)
+            ComponentDetail(type = stringResource(R.string.climate), value = planet.climate)
+            ComponentDetail(type = stringResource(R.string.gravity), value = planet.gravity)
+            ComponentDetail(type = stringResource(R.string.terrain), value = planet.terrain)
+            ComponentDetail(type = stringResource(R.string.surface_water), value = planet.surface_water)
+            ComponentDetail(type = stringResource(R.string.population), value = planet.population)
+            ComponentDetail(type = stringResource(R.string.residents), value = planet.residents.size.toString())
+            ComponentDetail(type = stringResource(R.string.films), value = planet.films.size.toString())
+        }
 
+    }
 }
--- app/src/main/java/com/mrkevin574/starwars/presentation/screens/planet/PlanetScreen.kt
+++ app/src/main/java/com/mrkevin574/starwars/presentation/screens/planet/PlanetScreen.kt
@@ -1,30 +1,84 @@
 package com.mrkevin574.starwars.presentation.screens.planet
 
+import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.google.gson.Gson
+import com.mrkevin574.starwars.R
+import com.mrkevin574.starwars.presentation.Event
 import com.mrkevin574.starwars.presentation.StarWarsViewModel
+import com.mrkevin574.starwars.presentation.components.ErrorMessageScreen
+import com.mrkevin574.starwars.presentation.components.Loading
+import com.mrkevin574.starwars.presentation.components.OptionSearch
+import com.mrkevin574.starwars.presentation.screens.people.ContentPeople
+import com.mrkevin574.starwars.presentation.screens.people.PeopleCard
+import com.mrkevin574.starwars.presentation.states.PeoplesState
+import com.mrkevin574.starwars.presentation.states.PlanetsState
+import com.mrkevin574.starwars.presentation.ui.theme.Black700
+import com.mrkevin574.starwars.util.Screens
 
 @Composable
 fun PlanetScreen(
     viewModel: StarWarsViewModel = hiltViewModel(),
-    navController: NavController
+    navController: NavHostController
 )
 {
-    Text(
-        text = "Planets",
-        modifier = Modifier.fillMaxSize().background(Color.Black),
-        textAlign = TextAlign.Center,
-        fontSize = 28.sp,
-        fontWeight = FontWeight.Bold,
-        color = Color.White
+    val planetState = viewModel.planets.value
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            OptionSearch(
+                label = stringResource(id = R.string.search_planet),
+                onSearch = { viewModel.onEvent(Event.SearchPeople(it)) }
+            )
+        },
+        content = { paddingValues ->
+            ContentPlanet(
+                paddingValues = paddingValues,
+                planetState = planetState,
+                navController = navController
+            )
+        },
+        backgroundColor = Color.Black
     )
+}
+
+@Composable
+fun ContentPlanet(
+    planetState: PlanetsState,
+    paddingValues: PaddingValues,
+    navController: NavHostController,
+) {
+    if (!planetState.error.isError && planetState.planets.isEmpty()) {
+        Loading(paddingValues)
+    } else if (planetState.error.isError) {
+        ErrorMessageScreen(message = planetState.error.errorMessage)
+    } else {
+        LazyColumn(contentPadding = paddingValues, modifier = Modifier.background(Black700))
+        {
+            items(planetState.planets)
+            {
+                PlanetCard(planet = it) {
+                    val route = Screens.PlanetDetailScreen.passFilm(Uri.encode(Gson().toJson(it)))
+                    navController.navigate(route)
+                }
+            }
+        }
+    }
 }
