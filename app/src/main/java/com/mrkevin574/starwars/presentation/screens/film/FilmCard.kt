60e7125384eec2e9252428b29093c4c0ea5096b3
MrKevin574 <mrkevin574@gmail.com>
2022-06-23 23:17:08 -0400

    V1 FilmCard

--- app/src/main/java/com/mrkevin574/starwars/presentation/screens/film/FilmCard.kt
+++ app/src/main/java/com/mrkevin574/starwars/presentation/screens/film/FilmCard.kt
@@ -1,14 +1,25 @@
 package com.mrkevin574.starwars.presentation.screens.film
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mrkevin574.starwars.domain.model.Film
+import com.mrkevin574.starwars.presentation.ui.theme.Black400
+import com.mrkevin574.starwars.presentation.ui.theme.YellowStarWars
+import com.mrkevin574.starwars.presentation.ui.theme.YellowStarWarsSecondary
+import com.mrkevin574.starwars.presentation.ui.theme.starWarsFont
 
 val testFilm = Film(
     title = "A New Hope",
@@ -34,24 +45,58 @@ fun FilmCard(
 {
     Card(
         modifier = Modifier
-            .height(400.dp)
             .fillMaxWidth()
             .padding(
                 start = 20.dp,
                 end = 20.dp,
-                top = 30.dp
+                top = 30.dp,
+                bottom = 15.dp
             ),
-        content = { ContentCard()}
+        border = BorderStroke(3.dp, YellowStarWars),
+        elevation = 8.dp,
+        content = { ContentCard(film)}
     )
 }
 
 @Composable
-fun ContentCard()
+fun ContentCard(film : Film)
 {
+    Column(
+        modifier = Modifier.background(Black400)
+    ) {
+        Text(
+            text = film.title,
+            color = YellowStarWars,
+            fontSize = 22.sp,
+            modifier = Modifier.fillMaxWidth()
+                .padding(10.dp),
+            textAlign = TextAlign.Center,
+            fontFamily = starWarsFont
+        )
 
+        Text(
+            text = film.opening_crawl,
+            color = Color.Gray,
+            fontSize = 10.sp,
+            modifier = Modifier.fillMaxWidth()
+                .padding(18.dp),
+            textAlign = TextAlign.Center,
+            fontFamily = starWarsFont
+        )
+
+        Text(
+            text = film.release_date,
+            color = YellowStarWarsSecondary,
+            fontSize = 16.sp,
+            modifier = Modifier.fillMaxWidth()
+                .padding(18.dp),
+            textAlign = TextAlign.Center,
+            fontFamily = starWarsFont
+        )
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewFilmCard()
 {
